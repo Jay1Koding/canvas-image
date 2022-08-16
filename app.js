6b0831ed2913388426071d51ee561ccb2ef2e2ca
jay1coding <jay1coding@gmail.com>
2022-08-16 14:50:56 +0900

    add reset

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const resetBtn = document.getElementById('reset');
 const colorInput = document.getElementById('color');
 const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
@@ -42,11 +43,18 @@ function onChangeColor(event) {
   ctx.fillStyle = currentColor;
 }
 
+function onClickReset() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
-// Toggle Filling or Painting
-// canvas.addEventListener('click', onCanvasClick);
 lineWidth.addEventListener('change', onChangeLineWidth);
 colorInput.addEventListener('change', onChangeColor);
+
+// Toggle Filling or Painting
+// canvas.addEventListener('click', onCanvasClick);
+resetBtn.addEventListener('click', onClickReset);
