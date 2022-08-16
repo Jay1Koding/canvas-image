d448e7ebba413332d0b843715577d4954b7618bc
jay1coding <jay1coding@gmail.com>
2022-08-16 14:22:06 +0900

    add linewidth change

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+const colorInput = document.getElementById('color');
+const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
+
 let isPainting = false;
 let isFilling = false;
 
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
+
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = lineWidth.value;
 
 function onMove(event) {
   if (isPainting) {
@@ -20,14 +24,29 @@ function onMove(event) {
 function startPainting() {
   isPainting = true;
 }
-
 function cancelPainting() {
   isPainting = false;
   ctx.beginPath();
+}
+
+function onChangeLineWidth(event) {
+  //   console.log(event.target.value);
+  const currentWidth = event.target.value;
+  ctx.lineWidth = currentWidth;
+}
+
+function onChangeColor(event) {
+  console.log(event.target.value);
+  const currentColor = event.target.value;
+  ctx.strokeStyle = currentColor;
+  ctx.fillStyle = currentColor;
 }
 
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
+// Toggle Filling or Painting
 // canvas.addEventListener('click', onCanvasClick);
+lineWidth.addEventListener('change', onChangeLineWidth);
+colorInput.addEventListener('change', onChangeColor);
